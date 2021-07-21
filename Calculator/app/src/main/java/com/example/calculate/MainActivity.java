5ada8b0dd55e4abde9110d616671f01e6196f7f4
tiddster <864187149@qq.com>
2021-07-21 12:30:34 +0800

    first

---
+++ Calculator/app/src/main/java/com/example/calculate/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.calculate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+}
